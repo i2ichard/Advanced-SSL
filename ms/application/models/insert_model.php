72b55d0ee606e0920722512173bc7d253f4080f3
Richard Mipana <richard.mipana@fullsail.edu>
2015-05-08 16:23:15 -0500

    query to insert into db

---
+++ ms/application/models/insert_model.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class Insert_model extends CI_Model{
+	function __construct() {
+		parent::__construct();
+	}
+	function form_insert($data){
+		$this->db->insert('data', $data);
+	}
+
+}
